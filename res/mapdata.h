a072e384019836069f454b52ef89fb8b29be4d1f
Michael Moffitt <Mike_Moffitt@intuit.com>
2015-09-08 12:31:28 -0700

    Updated mac makefile

--- res/mapdata.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MAPDATA_H_
-#define _MAPDATA_H_
-
-extern const u8 mapdata_startroom[13068];
-extern const u8 mapdata_sidesquare[10668];
-extern const u8 mapdata_roomzero[3468];
-extern const u8 mapdata_talltest[8268];
-
-#endif // _MAPDATA_H_
