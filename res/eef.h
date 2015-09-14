a914a0219a469a46338a54d41d55bd493251a8b9
unknown <mikejmoffitt@gmail.com>
2015-09-13 19:24:35 -0700

    More music updates

--- res/eef.h
+++ res/eef.h
@@ -2,5 +2,6 @@
 #define _EEF_H_
 
 extern const u8 eef_flat[4];
+extern const u8 eef_hat[8];
 
 #endif // _EEF_H_
--- res/eef.s
+++ res/eef.s
@@ -5,3 +5,8 @@
 eef_flat:
     dc.w    0xFE00, 0xFF00
 
+    .align  2
+    .global eef_hat
+eef_hat:
+    dc.w    0xFE07, 0x0A0C, 0x0D0E, 0x0FFF
+
