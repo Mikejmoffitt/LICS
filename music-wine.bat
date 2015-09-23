76a6c6946a935726dd90d3cedd3211e962ac0024
Michael Moffitt <mikejmoffitt@gmail.com>
2015-09-23 10:16:34 -0700

    Made music compilation possible under Linux/OSX

---
+++ music-wine.bat
@@ -0,0 +1,44 @@
+@echo off
+REM removing old object files
+del out\res\bgm.o /S /Q
+del out\res\eif.o /S /Q
+del out\res\eef.o /S /Q
+del out\res\ewf.o /S /Q
+
+REM convert raw 8-bit PCM files to EWF format
+echo ---------------------------------------------------------------------
+echo Converting samples to EWF
+echo ---------------------------------------------------------------------
+util\pcm2ewf.exe xm\rawsamples\snare1.raw res\ewf\snare1.ewf
+util\pcm2ewf.exe xm\rawsamples\snare2.raw res\ewf\snare2.ewf
+util\pcm2ewf.exe xm\rawsamples\bassdrum1.raw res\ewf\bassdrum1.ewf
+util\pcm2ewf.exe xm\rawsamples\hat1.raw res\ewf\hat1.ewf
+
+REM convert VGI instruments to EIF instruments
+echo ---------------------------------------------------------------------
+echo Converting VGI patches to EIF
+echo ---------------------------------------------------------------------
+util\vgi2eif.exe xm\inst\buzz.vgi res\eif\buzz.eif
+util\vgi2eif.exe xm\inst\bass1.vgi res\eif\bass1.eif
+util\vgi2eif.exe xm\inst\bass2.vgi res\eif\bass2.eif
+util\vgi2eif.exe xm\inst\sqchords1.vgi res\eif\sqchords1.eif
+util\vgi2eif.exe xm\inst\sawlead.vgi res\eif\sawlead.eif
+util\vgi2eif.exe xm\inst\saw2.vgi res\eif\saw2.eif
+util\vgi2eif.exe xm\inst\saw2at.vgi res\eif\saw2at.eif
+
+REM Sound effects
+echo ---------------------------------------------------------------------
+echo Beginning xm2esf conversions
+echo ---------------------------------------------------------------------
+cd util
+xm2esf Z:\home\moffitt\Development\LICS\xm\sfx\sfx_jump.xif Z:\home\moffitt\Development\LICS\res\sfx\jump.esf
+xm2esf Z:\home\moffitt\Development\LICS\xm\sfx\sfx_walk1.xif Z:\home\moffitt\Development\LICS\res\sfx\walk1.esf
+xm2esf Z:\home\moffitt\Development\LICS\xm\sfx\sfx_walk2.xif Z:\home\moffitt\Development\LICS\res\sfx\walk2.esf
+
+REM Now for the BGM
+
+xm2esf Z:\home\moffitt\Development\LICS\xm\bgm1\bgm1.xif Z:\home\moffitt\Development\LICS\res\bgm\bgm1.esf
+xm2esf Z:\home\moffitt\Development\LICS\xm\bgm2\bgm2.xif Z:\home\moffitt\Development\LICS\res\bgm\bgm2.esf
+
+REM clean up the working dir (oh god)
+cd ..
