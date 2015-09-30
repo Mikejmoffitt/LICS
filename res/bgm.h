4e9f5eecfde6295476ac61cf51969787854ecde2
Mikejmoffitt <mikejmoffitt@gmail.com>
2015-09-30 00:30:41 -0700

    More music work

--- res/bgm.h
+++ res/bgm.h
@@ -2,7 +2,7 @@
 #define _BGM_H_
 
 extern const u8 bgm_bgm1[43276];
-extern const u8 bgm_bgm2[24934];
+extern const u8 bgm_bgm2[24960];
 extern const u8 bgm_bgm3[26540];
 
 #endif // _BGM_H_
--- res/eef.h
+++ res/eef.h
@@ -3,6 +3,6 @@
 
 extern const u8 eef_flat[4];
 extern const u8 eef_hat[8];
-extern const u8 eef_qdecay[8];
+extern const u8 eef_qdecay[32];
 
 #endif // _EEF_H_
