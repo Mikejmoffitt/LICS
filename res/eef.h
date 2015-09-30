a0d34de8c9411d7cd64cd5c56fe660bde1ab5b2a
Mikejmoffitt <mikejmoffitt@gmail.com>
2015-09-30 00:13:13 -0700

    BGM1 work

--- res/eef.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EEF_H_
-#define _EEF_H_
-
-extern const u8 eef_flat[4];
-extern const u8 eef_hat[8];
-extern const u8 eef_qdecay[8];
-
-#endif // _EEF_H_
--- res/sfx.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SFX_H_
-#define _SFX_H_
-
-extern const u8 sfx_jump[130];
-extern const u8 sfx_walk1[514];
-extern const u8 sfx_walk2[514];
-
-#endif // _SFX_H_
