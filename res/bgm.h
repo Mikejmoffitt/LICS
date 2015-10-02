043342e3ebf75fcedc1ec74544fc6600f21455fc
Mikejmoffitt <mikejmoffitt@gmail.com>
2015-10-01 22:59:05 -0700

    Yet more fixes for BGM4

--- res/bgm.h
+++ res/bgm.h
@@ -4,6 +4,6 @@
 extern const u8 bgm_bgm1[43276];
 extern const u8 bgm_bgm2[24024];
 extern const u8 bgm_bgm3[23738];
-extern const u8 bgm_bgm4[37392];
+extern const u8 bgm_bgm4[37022];
 
 #endif // _BGM_H_
