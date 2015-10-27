0b8e2dc10a80e7cf0b5639518979825a9f412288
Michael Moffitt <Mike_Moffitt@intuit.com>
2015-10-27 13:23:38 -0700

    GCCErrC updated.

--- gccerrc.py
+++ gccerrc.py
@@ -1,30 +1,37 @@
-# Really simple GCC error output coloring script
+# Simple GCC error / warning colorization script
 # Michael Moffitt 2015
 
 import fileinput
 import sys
 import re
 
-def main():
+def line_parse(words):
+	# Print color control characters if a key word was identified
+	for word in words:
+		# Default to "bright white"
+		print ('\033[1m', end = "");
+		if ("error" in word):
+			# Red color
+			print ('\033[91m', end = "")
+		elif ("warning" in word):
+			# Yellow color
+			print ('\033[93m', end = "")
+		else:
+			# No color
+			print ('\033[0m', end = "")
+
+		# reinsert the removed space if necessary
+		if ('\n' not in word):
+			print (word, end=" ")
+		else:
+			print (word, end="")
+
+def main():	
+	# Reset color at start, just in case
+	print ('\033[0m', end="");
+
 	for line in fileinput.input():
-		words = re.split(' ',line);
-		for word in words:
-			# Everything is bold		
-			print ('\033[1m',end="")
-			if ("error" in word):
-				# Red color
-				print ('\033[91m',end="")
-			elif ("warning" in word):
-				# Yellow color
-				print ('\033[93m',end="")
-			else:
-				# No color
-				print ('\033[0m',end="")
-			# reinsert the removed space if necessary
-			if ('\n' not in word):
-				print (word, end=" ")
-			else:
-				print (word, end="")
-	
+		line_parse(re.split(' ', line));
+			
 if __name__ == "__main__":
 	main()
