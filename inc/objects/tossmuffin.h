b028655ccf7aacb43d276ffe23550bd116338b3c
Mikejmoffitt <mikejmoffitt@gmail.com>
2015-11-18 00:11:37 -0800

    Tossmuffin is working.

--- inc/objects/tossmuffin.h
+++ inc/objects/tossmuffin.h
@@ -3,6 +3,7 @@
 
 #include <genesis.h>
 #include "enemy.h"
+#include "cubes.h"
 
 // Structure defining specific object type
 typedef struct en_tossmuffin en_tossmuffin;
@@ -10,6 +11,13 @@ struct en_tossmuffin
 {	
 	// Header present in each drawable and processable enemy type
 	en_header head;
+	cube *holding_cube;
+	u16 anim_frame;
+	u16 anim_cnt;
+	u16 lift_cnt;
+	u16 toss_cnt;
+	u16 walk_delay;
+	u16 saw_player;
 };
 
 // Initialize enemy at address e
